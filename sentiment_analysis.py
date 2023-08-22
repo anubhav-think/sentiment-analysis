42d05b5a9f4fed82a2b0667d1f1cf31e47d721c3
Anubhav Srivastav <anubhav.srivastav.think@gmail.com>
2023-08-22 14:14:54 +0530

    updated code

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -9,11 +9,11 @@ tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-senti
 
 sentiment_task = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer,return_all_scores = True)
 
-#data =[{'body':text},{'body':text},..........]
+#data =[text,text,..........]
 def inference(data):
   test = []
   for i in data:
-    text = i['body'].replace('\n',' ')
+    text = i.replace('\n',' ')
     res = sentiment_task(text)
     test.append({'comment_text':text,res[0][0]['label']:res[0][0]['score'],res[0][1]['label']:res[0][1]['score'],res[0][2]['label']:res[0][2]['score']})
 
@@ -22,11 +22,19 @@ def inference(data):
 
 
 
-url = 'https://api.pullpush.io/reddit/search/comment/?q=toyota&before=1680303707'
+def get_data(url):
+  data =  requests.get(url)
+  data = data.json()
+  return data['data']
 
-data =  requests.get(url)
-data = data.json()
+main = []
+utc = "1680303707"
+for i in range(100):
+  url = f'https://api.pullpush.io/reddit/search/comment/?q=toyota&before={utc}'
+  temp = get_data(url)
+  for index,i in enumerate(temp):
+    main.append(i['body'])
+    if i==99:
+      utc = i['created_utc']-1
 
-data = data['data']
-inference(data)
-
+inference(main)
